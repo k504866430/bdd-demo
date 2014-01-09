a32274deeeb6b6338bd93ba63c15ec9be8e8623b
Kruttik Aggarwal <kruttik.aggarwal@localhost.(none)>
2014-01-09 02:07:40 -0500

    Removed Files

---
+++ src/test/groovy/com/lab49/techbreakfast/cuke/stepdefinitions/Steps.groovy
@@ -0,0 +1,5 @@
+package com.lab49.techbreakfast.cuke.stepdefinitions
+// Add functions to register hooks and steps to this script.
+this.metaClass.mixin(cucumber.api.groovy.Hooks)
+this.metaClass.mixin(cucumber.api.groovy.EN)
+this.metaClass.mixin(geb.Browser)
